39a80209a420ce1a08ea8c8389f5b71ed36289d9
abruner <abruner1990@outlook.com>
2016-04-17 23:06:21 -0400

    Update objectc.m

--- objectc.m
+++ objectc.m
@@ -3,3 +3,12 @@
 #import <Foundation/Foundation.h>
 
 int main (int agrc, const char * argv[])
+
+{
+
+    NSAutoreleasePool *pool = [(NSAutoreleasePool alloc] init];
+    NSLog (@"Hello, World!");
+    [pool drain];
+    return 0;
+    
+    }
